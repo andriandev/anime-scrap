b16dbc37f5a6918234f0271ca63d10b6541cc9c1
Andrian Dev <andrianpoint@gmail.com>
2022-04-13 09:05:59 +0700

    Memperbaiki beberapa fitur dan tampilan halaman

---
+++ public/assets/js/script.js
@@ -0,0 +1,5 @@
+(function () {
+document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+})
+})();
