71b04d4ae26e0f632b26f9f7d556e92573dcfb23
Andrian Dev <andrianpoint@gmail.com>
2022-04-20 08:29:46 +0700

    Menambahkan scrap oploverz

--- app/controllers/scrap/scrap.js
+++ app/controllers/scrap/scrap.js
@@ -11,7 +11,47 @@ exports.Index = (req, res) => {
 exports.ScrapEps = (req, res) => {
     if(req.query.url == '' || !req.query.url) {
         res.json('Empty Urls');
-    } else {
+    } else if(req.query.url.includes('oploverz')) {
+        (async () => {
+            const config = {
+                baseSiteUrl: `https://oploverz.asia/`,
+                startUrl: req.query.url,
+                concurrency: 5, //Maximum concurrent jobs. More than 10 is not recommended.Default is 3.
+                maxRetries: 3, //The scraper will try to repeat a failed request few times(excluding 404). Default is 5.       
+                showConsoleLogs: false,
+                logPath: null, //Highly recommended: Creates a friendly JSON for each operation object, with all the relevant data.
+            }
+
+            function getElementContent(element){
+                return `<div class="bixbox">${element}</div>`;
+            } 
+        
+            const scraper = new Scraper(config); //Create a new Scraper instance, and pass config to it.
+        
+            //Now we create the "operations" we need:
+        
+            const root = new Root(); //The root object fetches the startUrl, and starts the process.  
+        
+            const link = new CollectContent('.entry-content .bixbox', { name: 'link', contentType:'html', getElementContent }); 
+        
+            root.addOperation(link);
+        
+            await scraper.scrape(root);
+        
+            try {
+                const links = link.getData();
+
+                if (links == '') {
+                    res.json('Invalid url.');
+                    return false;
+                }
+
+                res.json(links.at(-1)); 
+            } catch (e) {
+                res.json(e);
+            } 
+        })();  
+    } else if(req.query.url.includes('otakudesu')) {
         (async () => {
             const config = {
                 baseSiteUrl: `https://otakudesu.site/`,
@@ -57,7 +97,59 @@ exports.ScrapEps = (req, res) => {
 exports.ScrapAlleps = (req, res) => {
     if(req.query.url == '' || !req.query.url) {
         res.json('Empty All Urls');
-    } else {
+    } else if(req.query.url.includes('oploverz')) {
+        ( async () => {
+            const config = {
+                baseSiteUrl: `https://oploverz.asia/`,
+                startUrl: req.query.url,
+                concurrency: 1, //Maximum concurrent jobs. More than 10 is not recommended.Default is 3.
+                maxRetries: 3, //The scraper will try to repeat a failed request few times(excluding 404). Default is 5.       
+                showConsoleLogs: false,
+                logPath: null, //Highly recommended: Creates a friendly JSON for each operation object, with all the relevant data.
+            }
+
+            function getElementContent(element){
+                return `<div class="bixbox">${element}</div>`;
+            }   
+
+            const scraper = new Scraper(config); //Create a new Scraper instance, and pass config to it.
+
+            //Now we create the "operations" we need:
+        
+            const root = new Root(); //The root object fetches the startUrl, and starts the process.  
+
+            const eps = new OpenLinks('article .bixbox.bxcl.epcheck .eplister ul li a', {name:'eps' }); //Opens each eps page.
+
+            const link = new CollectContent('.entry-content .bixbox', { name: 'link', contentType:'html', getElementContent }); //"Collects" the link dl from each
+
+            root.addOperation(eps); //Then we create a scraping "tree":
+                eps.addOperation(link);
+
+            await scraper.scrape(root);
+
+            try {
+                const Alleps = eps.getData(); 
+
+                if (Alleps == '') {
+                    res.json('Invalid url.');
+                    return false;
+                }
+
+                let AllData = [];
+                Alleps.forEach(el => {
+                    if(el.data[0].data.length == 3) {
+                        AllData.push(el.data[0].data[2]);
+                    } else if(el.data[0].data.length == 2) {
+                        AllData.push(el.data[0].data[1]);
+                    } 
+                });
+
+                res.json(AllData.reverse().join(' '));
+            } catch (e) {
+                res.json(e);
+            }
+        })();
+    } else if(req.query.url.includes('otakudesu')) {
         ( async () => {
             const config = {
                 baseSiteUrl: `https://otakudesu.site/`,
